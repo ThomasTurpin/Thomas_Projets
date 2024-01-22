8d75d5d984ccfd6c5c20d4ada90d71e881a25e3a
Thomas_T <71838275+ThomasTurpin@users.noreply.github.com>
2024-01-22 12:40:10 +0100

    Update search_and_replace.c

--- EXAM/EXAM_02/lvl.01/search_and_replace.c
+++ EXAM/EXAM_02/lvl.01/search_and_replace.c
@@ -5,7 +5,7 @@ int	main(int argc, char **argv)
 	int	i;
 
 	i = 0;
-	if (argc == 4 && !argv[2][1] & !argv[3][1])
+	if (argc == 4 && !argv[2][1] && !argv[3][1])
 	{
 		while (argv[1][i] != '\0')
 		{
